9318a6b45c70077ec96b0958d05caff96b094b4c
rathnayr <rathnayr@metropolia.fi>
2024-09-01 00:16:22 +0530

    Assignment_04

--- Task_02.py
+++ Task_02.py
@@ -3,7 +3,7 @@ while True:
     Inches=float(input("Enter a value to convert:"))
 
     if Inches<0:
-        print("You have entered a negative values. Program is ended")
+        print("You have entered a negative value. Program is ended")
         break
     Centimeters=Inches*Inches_to_Centimeters
     print(f"Entered {Inches} equal to {Centimeters}")
