6ed6852118470c676be500e555bc8fd3d746fca2
rathnayr <rathnayr@metropolia.fi>
2024-09-01 02:13:37 +0530

    Assignment_04

--- Task_04.py
+++ Task_04.py
@@ -3,12 +3,17 @@ Number = random.randint(1,10)
 while True:
     try:
         User_Guess = int(input("Enter a number between 1 and 10: "))
+
+        if User_Guess <1 or User_Guess >10:
+            print("Enter a number between 1 and 10")
+            continue
         if User_Guess < Number:
-            print ("Your guess is too low")
+            print ("Your guess is Too Low")
         elif User_Guess > Number:
-            print ("Your guess is too high")
+            print ("Your guess is Too High")
         else:
-            print ("You guess is correct")
+            print ("You guess is Correct")
+            print("Thanks for playing")
             break
     except ValueError:
         print("Please enter a number between 1 and 10")
